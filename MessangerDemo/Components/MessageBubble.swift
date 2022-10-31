4001e5c95514ee8c420b20b39c9e36d18976d4ca
Oleg Fedotov <trevor706@gmail.com>
2022-10-31 21:12:44 +0300

    MessageBubble

--- MessangerDemo/Components/MessageBubble.swift
+++ MessangerDemo/Components/MessageBubble.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct MessageBubble: View {
     var message: Message
+    @State private var showTime = false
     
     var body: some View {
         VStack(alignment: message.recieved ? .leading : .trailing) {
             HStack {
                 Text(message.text)
+                    .padding()
+                    .background(message.recieved ? Color("Gray") : Color("Violet"))
+                    .foregroundColor(message.recieved ? .black : .white)
+                    .cornerRadius(30)
             }
+            .frame(maxWidth: 300, alignment: message.recieved ? .leading : .trailing)
+            .onTapGesture {
+                showTime.toggle()
+            }
+            
+            if showTime {
+                Text("\(message.timestamp.formatted(.dateTime.hour().minute()))")
+                    .font(.system(size: 12, weight: .medium))
+                    .foregroundColor(.gray)
+                    .padding(message.recieved ? .leading : .trailing, 20)
+            }
+            
         }
+        .frame(maxWidth: .infinity, alignment: message.recieved ? .leading : .trailing)
+        .padding(message.recieved ? .leading : .trailing)
+        .padding(.horizontal, 10)
     }
 }
 
