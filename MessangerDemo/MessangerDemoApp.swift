208f9a08408d42150313fd0cf47b29f7b0f8f64c
Oleg Fedotov <trevor706@gmail.com>
2022-10-31 19:41:27 +0300

    Initial Commit

---
+++ MessangerDemo/MessangerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessangerDemoApp.swift
+//  MessangerDemo
+//
+//  Created by Olegio on 31.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MessangerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
