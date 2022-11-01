1ccf93b7d00fa2ef6c426fdcb193b40874c70799
Oleg Fedotov <trevor706@gmail.com>
2022-11-01 11:57:53 +0300

    MessageField

--- MessangerDemo/ContentView.swift
+++ MessangerDemo/ContentView.swift
@@ -30,6 +30,7 @@ struct ContentView: View {
             }
             .padding(.top, 10)
             .background(.white)
+            .cornerRadius(30, corners: [.topLeft, .topRight])
         }
         .background(Color("Violet"))
     }
